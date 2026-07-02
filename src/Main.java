f06b26a13920c7a12e3c88cc64f2debf1f4c7d58
Bhavya Shukla <bhavyashukla560@gmail.com>
2026-07-02 16:31:41 +0530

    Add Student class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Student student = new Student(101, "Bhavya", 92);
+
+        System.out.println(student);
+
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,60 @@
 public class Student {
+    private int id;
+    private String name;
+    private double marks;
+    private char grade;
+
+    public Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+        this.grade = calculateGrade();
+    }
+
+    private char calculateGrade() {
+
+        if (marks >= 90)
+            return 'A';
+        else if (marks >= 80)
+            return 'B';
+        else if (marks >= 70)
+            return 'C';
+        else if (marks >= 60)
+            return 'D';
+        else
+            return 'F';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+        this.grade = calculateGrade();
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                " | Name: " + name +
+                " | Marks: " + marks +
+                " | Grade: " + grade;
+    }
 }
